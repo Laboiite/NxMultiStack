342901280d7e1b837280649dbcb549c28da9ad41
Laboiite <>
2022-12-10 18:54:41 +0100

    feat(portal): Init basic routing and titles
    Preparing lazy loading of our portal shell module
    Creating our portal-container component, that will later host dynamic cards to redirect to applications
    
    Signed-off-by: Laboiite <>

--- apps/portal/src/app/app.routes.ts
+++ apps/portal/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'portal',
+      },
+      {
+        path: 'portal',
+        loadChildren: () =>
+          import('@nxmultistack/portal/shell').then(m => m.PortalShellModule),
+      },
+];
--- libs/portal/shell/src/lib/portal-shell.module.ts
+++ libs/portal/shell/src/lib/portal-shell.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { PortalContainerComponent } from './portal-container/portal-container.component';
 
+const appRoutes: Route[] = [
+  {
+    path: '',
+    component: PortalContainerComponent,
+  },
+];
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(appRoutes),
+  ],
+  declarations: [PortalContainerComponent],
 })
 export class PortalShellModule {}
